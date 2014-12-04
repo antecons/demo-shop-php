04a09f5f27289fb6a13d28f56ab69a387eba74de
David Volquartz Lebech <david@lebech.info>
2014-12-04 21:17:32 +0100

    Fix missing php tag

--- inc/html/product_add_to_cart.php
+++ inc/html/product_add_to_cart.php
@@ -3,7 +3,7 @@
         <input type="hidden" name="action" value="add_cart">
         <button class="btn btn-primary" type="submit">Add to cart</button>
     </form>
-<?php if ($addedToCart) { ?>
+<?php if (isset($addedToCart) && $addedToCart) { ?>
     <p>The product was added to the <a href="/cart">cart</a>.</p>
-<? } ?>
+<?php } ?>
 </div>
